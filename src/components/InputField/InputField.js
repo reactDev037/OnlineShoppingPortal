c97db1b5c483d8c372560fb7442ff8c2254ef752
morgan <developerit037@gmail.com>
2018-09-22 17:48:56 +0530

    Custom input component.

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -3,14 +3,15 @@ import "./InputField.css";
 export default class InputField extends Component {
 	render() {
 		return (
-			<div className="input-field">
+			<label className="input-field">
 				<input
 					className="input-raw"
 					value={this.props.value}
 					onChange={this.props.onChange}
 					type={this.props.type || "text"}
 				/>
-			</div>
+				<div className="actual-label">{this.props.label}</div>
+			</label>
 		);
 	}
 }
